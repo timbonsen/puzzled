55255ad8c416e068cccd6f720a0ee9fd2dc4108b
timbonsen <73481594+timbonsen@users.noreply.github.com>
2021-05-11 10:31:43 +0200

    Login Header conditioneel gemaakt.

--- src/components/LoginHeader.js
+++ src/components/LoginHeader.js
@@ -1,18 +1,24 @@
 import {Link} from "react-router-dom";
+import {useContext} from "react";
+import {AuthContext} from "../context/AuthContext";
 
 function LoginHeader() {
 
-    return (
-        <h3>
-            <div className="pageLink">
-                <Link to="/signin">INLOGGEN</Link>
-            </div>
-             OF
-            <div className="pageLink">
-                <Link to="/signup">AANMELDEN</Link>
-            </div>
-        </h3>
+    const { user } = useContext(AuthContext);
+
+    if (user === null || user === undefined) {
+        return (
+            <h3>
+                <div className="pageLink">
+                    <Link to="/signin">INLOGGEN</Link>
+                </div>
+                OF
+                <div className="pageLink">
+                    <Link to="/signup">AANMELDEN</Link>
+                </div>
+            </h3>
         )
+    } return (<></>)
 }
 
 export default LoginHeader;
