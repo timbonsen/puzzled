332a1ec07d5ba800f7db07690cd7e3e79cc76fbe
timbonsen <73481594+timbonsen@users.noreply.github.com>
2021-05-11 10:31:27 +0200

    Private Routes aangemaakt

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ExchangeProposal from "./pages/exchange/ExchangeProposal";
 import ExchangeAccepted from "./pages/exchange/ExchangeAccepted";
 import RegisterAddress from "./pages/registerAddress/RegisterAddress";
 import HasLoggedOut from "./pages/hasLoggedOut/HasLoggedOut";
+import PrivateRoute from "./components/PrivateRoute";
 
 
 function App() {
@@ -34,7 +35,9 @@ function App() {
                     <SearchPage/>
                 </Route>
                 <Route path="/upload">
-                    <UploadPage/>
+                    <PrivateRoute
+                        page={<UploadPage/>}
+                        />
                 </Route>
                 <Route path="/account">
                     <AccountPage/>
@@ -43,13 +46,19 @@ function App() {
                     <PuzzlePage/>
                 </Route>
                 <Route exact path="/exchange/proposal">
-                    <ExchangeProposal />
+                    <PrivateRoute
+                        page={<ExchangeProposal />}
+                        />
                 </Route>
                 <Route exact path="/exchange/accepted">
-                    <ExchangeAccepted />
+                    <PrivateRoute
+                        page={<ExchangeAccepted />}
+                    />
                 </Route>
                 <Route path="/register-address">
-                    <RegisterAddress />
+                    <PrivateRoute
+                        page={<RegisterAddress />}
+                        />
                 </Route>
                 <Route path="/logged-out">
                     <HasLoggedOut />
---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,14 @@
+import {AuthContext} from "../context/AuthContext";
+import {useContext} from "react";
+import {Redirect} from "react-router-dom";
+
+function PrivateRoute({ page }) {
+    const { user } = useContext(AuthContext);
+
+    if (user != null) {
+        return page;
+    }
+    return <Redirect to="/" />
+}
+
+export default PrivateRoute;
