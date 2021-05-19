b15add0c7587f194c3a2dfb9a024ee0ef4310967
timbonsen <73481594+timbonsen@users.noreply.github.com>
2021-05-19 15:33:50 +0200

    Image Upload Context aangemaakt.

---
+++ src/context/ImageUploadContext.js
@@ -0,0 +1,36 @@
+import {createContext, useEffect, useState} from "react";
+
+export const ImageUploadContext = createContext({});
+
+function ImageUploadContextProvider({children}) {
+    const [imageId, setImageId] = useState(null)
+
+    function registerImageId(registeredId) {
+        /*console.log(registeredId);*/
+        setImageId(registeredId);
+        /*console.log(imageId);*/
+    }
+
+    useEffect(() => {
+        console.log(imageId)
+    },[imageId]);
+
+    function deleteImageId() {
+        setImageId(null);
+    }
+
+    const data = {
+    ...imageId,
+        registerId: registerImageId,
+        deleteId: deleteImageId,
+
+    }
+
+    return (
+        <ImageUploadContext.Provider value={data}>
+            { children }
+        </ImageUploadContext.Provider>
+    )
+}
+
+export default ImageUploadContextProvider;
