09ab46fe27cee98edde8711d6fb2a9f0d7c3ed1c
timbonsen <73481594+timbonsen@users.noreply.github.com>
2021-05-24 22:47:30 +0200

    Admin check en pagina button toegevoegd.

--- src/components/topmenu/TopMenu.js
+++ src/components/topmenu/TopMenu.js
@@ -10,6 +10,27 @@ function TopMenu() {
     const { user } = useContext(AuthContext);
 
     if (user) {
+        if (user.username === "admin") {
+            return (
+                <div className="navContainer">
+                    <div className="navBar">
+                        <nav>
+                            <div className="logoContainer">
+                                <img className="puzzledLogo" src={logo} alt="Puzzled Logo"/>
+                                <NavLink exact to="/" className="puzzledName">PUZZLED</NavLink>
+                            </div>
+                            <NavLink exact to="/" className="navButton" activeClassName="navButtonActive">HOME</NavLink>
+                            <NavLink to="/upload" className="navButton" activeClassName="navButtonActive">UPLOAD</NavLink>
+                            <NavLink exact to="/search" className="navButton" activeClassName="navButtonActive">SEARCH</NavLink>
+                            <NavLink exact to="/admin" className="navButton" activeClassName="navButtonActive">ADMIN</NavLink>
+                        </nav>
+                        <NavLink to="/account" className="accountButton">
+                            <img className="accountButtonImage" src={accountButton} alt="Account Button"/>
+                        </NavLink>
+                    </div>
+                </div>
+            )
+        }
         return (
             <div className="navContainer">
                 <div className="navBar">
@@ -21,8 +42,6 @@ function TopMenu() {
                         <NavLink exact to="/" className="navButton" activeClassName="navButtonActive">HOME</NavLink>
                         <NavLink to="/upload" className="navButton" activeClassName="navButtonActive">UPLOAD</NavLink>
                         <NavLink exact to="/search" className="navButton" activeClassName="navButtonActive">SEARCH</NavLink>
-                        <NavLink exact to="/puzzle" className="navButton" activeClassName="navButtonActive">PUZZLE</NavLink>
-                        <NavLink to="/exchange/proposal" className="navButton" activeClassName="navButtonActive">EXCHANGE</NavLink>
                     </nav>
                     <NavLink to="/account" className="accountButton">
                         <img className="accountButtonImage" src={accountButton} alt="Account Button"/>
