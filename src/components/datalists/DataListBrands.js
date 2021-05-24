b4b3636e250c5e57270a0719335c441a23306436
timbonsen <73481594+timbonsen@users.noreply.github.com>
2021-05-24 22:45:51 +0200

    Datalist componenten gemaakt

---
+++ src/components/datalists/DataListBrands.js
@@ -0,0 +1,16 @@
+function DataListBrands() {
+    return (
+        <datalist id="brands">
+            <option>King</option>
+            <option>Ravensburger</option>
+            <option>Jumbo</option>
+            <option>Falcon</option>
+            <option>Goliath</option>
+            <option>Castorland</option>
+            <option>Clementoni</option>
+            <option>Gibsons</option>
+        </datalist>
+    )
+}
+
+export default DataListBrands;
---
+++ src/components/datalists/DataListNumberOfPieces.js
@@ -0,0 +1,16 @@
+function DataListNumberOfPieces() {
+    return (
+        <datalist id="pieces">
+            <option>25</option>
+            <option>50</option>
+            <option>100</option>
+            <option>200</option>
+            <option>500</option>
+            <option>1000</option>
+            <option>1500</option>
+            <option>2000</option>
+        </datalist>
+    )
+}
+
+export default DataListNumberOfPieces;
