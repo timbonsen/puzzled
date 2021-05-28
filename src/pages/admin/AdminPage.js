d56a78a74a6fe1218d1fafe89fa4936f4b64d71f
timbonsen <73481594+timbonsen@users.noreply.github.com>
2021-05-28 10:13:00 +0200

    Adminpagina toont lijst met users.

--- src/pages/admin/AdminPage.js
+++ src/pages/admin/AdminPage.js
@@ -1,17 +1,24 @@
-import PageHeader from "../../components/PageHeader";
-import {useEffect, useState} from "react";
-import https from "../../http-common"
+import PageHeader from "../../components/headers/PageHeader";
+import {useEffect} from "react";
+import https from "../../http-common";
+import './admin.css';
 
 function AdminPage() {
 
-    const [userList, setUserList] = useState("Hier komen de usernames");
-
     async function fetchUsers() {
         try {
-            const result = await https.get(`admin`);
+            const result = await https.get(`/admin/all-users`);
             console.log(result);
+            for (const user of result.data) {
+                const container = document.getElementById("userList");
+                const item = document.createElement("li");
+                item.setAttribute("class", "adminUserList");
+                item.textContent = user.toString();
+                container.appendChild(item);
+            }
         } catch (e) {
             console.error(e);
+            console.dir(e);
         }
     }
     useEffect(fetchUsers,[]);
@@ -21,7 +28,9 @@ function AdminPage() {
             <PageHeader title="admin" />
             <div className="pageContainer">
                 <div className="pageContent">
-                    {userList}
+                    <h3>Alle gebruikers van Puzzled</h3>
+                    <ul id="userList" className="userList">
+                    </ul>
                 </div>
             </div>
         </>
