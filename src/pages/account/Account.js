7544739cff6bb571af1ac573c19cbf725e9ed276
timbonsen <73481594+timbonsen@users.noreply.github.com>
2021-05-28 21:05:46 +0200

    Accountpagina gestyled.

--- src/pages/account/Account.js
+++ src/pages/account/Account.js
@@ -3,13 +3,13 @@ import PageHeader from "../../components/headers/PageHeader";
 import LoginHeader from "../../components/headers/LoginHeader";
 import {AuthContext} from "../../context/AuthContext";
 import IsAddressPresent from "../../components/functions/IsAddressPresent";
-import LinkButton from "../../components/buttons/linkButton/linkButton";
+import LinkButton from "../../components/buttons/linkButton";
 import DisplayPuzzles from "../../components/functions/DisplayPuzzles";
+import "./Account.css"
 
 function AccountPage() {
 
     const {user, logout} = useContext(AuthContext);
-    console.log(user)
 
     if (!user) {
         return (
@@ -18,6 +18,7 @@ function AccountPage() {
                 <div className="pageContainer">
                     <div className="pageContent">
                         <LoginHeader/>
+                        <p>Je bent nog niet ingelogd.</p>
                     </div>
                 </div>
             </>
@@ -44,19 +45,27 @@ function AccountPage() {
                 <PageHeader title={`account details ${user.username}`}/>
                 <div className="pageContainer">
                     <div className="pageContent">
-                        <h3>Mijn gegevens</h3>
-                        <p>
-                            Naam: {user.fullname}<br/>
-                            Emailadres: {user.email}
-                        </p>
-                        <h3>Mijn adres</h3>
-                        <IsAddressPresent/>
+                        <div className="accountDetailsContainer">
+                            <div className="accountDetails">
+                                <h3>Mijn gegevens</h3>
+                                <p>
+                                    Gebruikersnaam: {user.username}<br/>
+                                    Naam: {user.fullname}<br/>
+                                    Emailadres: {user.email}
+                                </p>
+                            </div>
+                            <div className="addressContainer">
+                                <h3>Mijn adres</h3>
+                                <IsAddressPresent/>
+                            </div>
+                        </div>
                         <h3>Mijn puzzels</h3>
-
+                        <LinkButton link="/upload" title="puzzel uploaden" />
                         <DisplayPuzzles search="user"/>
-
-                        <button className="regularButton" type="button" onClick={logout}>LOG UIT</button>
-                        <LinkButton link="/delete-account" title="verwijder account"/>
+                        <div className="buttonBar">
+                            <button className="regularButton" type="button" onClick={logout}>LOG UIT</button>
+                            <LinkButton link="/delete-account" title="verwijder account" buttonStyle="delete" />
+                        </div>
                     </div>
                 </div>
             </>
