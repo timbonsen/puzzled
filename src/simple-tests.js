59d1f0ae31c7d511a6b6b8ff4e9b0f36c89e2c2f
timbonsen <73481594+timbonsen@users.noreply.github.com>
2021-05-28 21:22:00 +0200

    Simpele tests geschreven.

--- src/simple-tests.js
+++ src/simple-tests.js
@@ -1,6 +1,20 @@
-
-function sortByUsername(array) {
-    array.sort();
-    return array;
+function add(num1, num2){
+    
+    return num1 + num2;
 }
 
+function findByName(users, name) {
+
+    for (let i = 0; i < users.length; i++) {
+        let user = users[i];
+        if (user.name === name) {
+            return user;
+        }
+    }
+    return null;
+}
+
+module.exports = {
+    add,
+    findByName,
+}
