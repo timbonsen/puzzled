bfc933aa76909845e6b9ab0523e69a8b8538a4fd
timbonsen <73481594+timbonsen@users.noreply.github.com>
2021-05-28 21:14:14 +0200

    Logs verwijderd.

--- src/components/puzzleDisplay/LargePuzzleCard.js
+++ src/components/puzzleDisplay/LargePuzzleCard.js
@@ -1,19 +1,15 @@
 import './puzzleCard.css'
-import {useHistory} from "react-router-dom";
+
 
 function PuzzleCard({ image, input }) {
-    console.log(image);
-    const history = useHistory();
     const imageFile = new File([image.data], input.title, {
         type: image.data.type
     });
-    console.log(imageFile);
     const imageUrl = window.webkitURL.createObjectURL(imageFile);
-    console.log(imageUrl);
 
     return (
         <div className="puzzleCard">
-            <img className="previewImage" src={imageUrl} alt={input.title} />
+            <img className="largeImage" src={imageUrl} alt={input.title} />
         </div>
     )
 }
