57fc17058062cb661f1b825caacaa86cc1ef6cb1
timbonsen <73481594+timbonsen@users.noreply.github.com>
2021-05-28 21:17:00 +0200

    Logs verwijderd.

--- src/components/puzzleDisplay/PuzzleCard.js
+++ src/components/puzzleDisplay/PuzzleCard.js
@@ -2,23 +2,19 @@ import './puzzleCard.css'
 import {useHistory} from "react-router-dom";
 
 function PuzzleCard({ image, input }) {
-    console.log(image);
     const history = useHistory();
     const imageFile = new File([image.data], input.title, {
         type: image.data.type
     });
-    console.log(imageFile);
     const imageUrl = window.webkitURL.createObjectURL(imageFile);
-    console.log(imageUrl);
 
     function displayPuzzle() {
-        console.log(input.id);
         history.push(`/puzzle/${input.id}`);
     }
 
     return (
         <div className="puzzleCard" onClick={displayPuzzle}>
-            <img className="puzzleImage" src={imageUrl} alt={input.title} />
+            <img className="puzzleImage" src={imageUrl} alt={input.title} key={imageUrl} />
         </div>
     )
 }
